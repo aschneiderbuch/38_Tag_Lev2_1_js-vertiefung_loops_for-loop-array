4934b9a58a697ac947468db304db729d55bec451
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-03 22:46:50 +0100

    testing

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,18 +25,18 @@ function imageArray() {
     console.log(returnArray);
 
     for (let rundenIndex = 1; rundenIndex < 101; rundenIndex = rundenIndex + 1) {
-        console.log(rundenIndex);
-        console.log(rundenIndex.toString().length);
+       console.log(rundenIndex);
+       console.log(rundenIndex.toString().length);
 
         // blanko Bild Name
         let image = "image_"
-        console.log(image);
+       console.log(image);
 
         // umbenennen ob 0 00 usw
         if (rundenIndex.toString().length == 1) {
             console.log(rundenIndex.toString());
             image = (`${image}00${rundenIndex}.jpg`);
-            console.log(image);
+         console.log(image);
         } else if (rundenIndex.toString().length == 2) {
             console.log(rundenIndex.toString());
             image = (`${image}0${rundenIndex}.jpg`);
@@ -53,7 +53,7 @@ returnArray.push(image);
 console.log(returnArray);
 console.table(returnArray);
 
-// jeder Array durchlauf
+// 100x jeder Array durchlauf
 // document.write(`${returnArray.join(" <br>")} `); 
 
 
